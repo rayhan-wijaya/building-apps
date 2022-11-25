7a7f2aaea822a7388b9b74bfeb73cddb9612650b
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-11-25 15:54:06 +0700

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next";
+
+import Head from "next/head";
+import Image from 'next/image';
+import Link from "next/link";
+
+import { trpc } from "../utils/trpc";
+
+const Home: NextPage = () => {
+	return (
+		<></>
+	);
+};
+
+export default Home;
