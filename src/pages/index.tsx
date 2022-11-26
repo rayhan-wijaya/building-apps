9e234f4d15ba205479e3f84c7af7a8dc644f74d2
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-11-26 14:43:00 +0700

    Features and lessons as static props in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next";
 
+import { GetStaticProps } from 'next';
 import Head from "next/head";
 
 import path from 'path';
@@ -10,10 +11,29 @@ import Headline from 'components/home_page/Headline';
 import Features from 'components/home_page/Features';
 import LessonMaterials from 'components/home_page/LessonMaterials';
 
-const Home: NextPage = () => {
+interface Props {
+	features: Feature[];
+	lessonMaterials: LessonMaterial[];
+}
+
+const Home: NextPage<Props> = ({ features, lessonMaterials }) => {
 	return (
 		<></>
 	);
 };
+
+export const getStaticProps: GetStaticProps = async (context) => {
+	const dataDir = path.join(process.cwd(), 'data');
+
+	const features = await readJson<Feature[]>('features.json', dataDir);
+	const lessonMaterials = await readJson<LessonMaterial[]>('lessonMaterials.json', dataDir);
+
+	return {
+		props: {
+			features,
+			lessonMaterials,
+		}
+	}
+}
 
 export default Home;
