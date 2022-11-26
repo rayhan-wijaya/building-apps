10436e8b091a213299a93f29993a944dbd564ea8
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-11-26 13:16:23 +0700

    Added colors to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,18 @@ module.exports = {
 	mode: 'jit',
 	content: ["./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'cool-blue': {
+					900: '#151A49',
+					800: '#2E3464',
+				},
+				'ice-blue': {
+					100: '#5FA8FF',
+					900: '#1D4676',
+				},
+			},
+		},
 	},
 	plugins: [],
 };
