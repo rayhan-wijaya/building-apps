81e59c5693dcb776b651de965ddb34b77f98d7b2
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-11-26 14:35:10 +0700

    Imported readJson in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,8 @@ import Head from "next/head";
 
 import path from 'path';
 
+import readJson from 'utils/readFile/readJson';
+
 import Headline from 'components/home_page/Headline';
 import Features from 'components/home_page/Features';
 import LessonMaterials from 'components/home_page/LessonMaterials';
