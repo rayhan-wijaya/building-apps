c7e8b574c640bfdfb0a708a8cbe3b7841afa8b81
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-11-26 13:33:15 +0700

    Removed unnecessary imports in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
 import { NextPage } from "next";
 
 import Head from "next/head";
-import Image from 'next/image';
-import Link from "next/link";
 
-import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
 	return (
