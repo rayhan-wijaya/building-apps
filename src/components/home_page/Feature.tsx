e1a05906010fa3714b2cd7f4b76e67395e2e3247
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-11-26 13:23:51 +0700

    Created feature component

---
+++ src/components/home_page/Feature.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import Image from 'next/image';
+
+interface Props {
+	feature: Feature;
+	className?: string;
+}
+
+const Feature = ({ feature, className }: Props) => {
+	return (
+		<div className={`bg-white rounded-lg p-12 shadow-lg flex gap-4 ${className}`}>
+			<div className={`flex justify-center object-contain`}>
+				<Image
+					src={`/features/${feature.image}` ?? ''}
+					alt={feature.imageAlt ?? 'A feature icon.'}
+					height={110}
+					width={110}
+				/>
+			</div>
+
+			<div>
+				<h3 className="text-center text-ice-blue-900 font-bold text-2xl mb-3">
+					{feature.title}
+				</h3>
+
+				<p className="text-center leading-5">{feature.description}</p>
+			</div>
+		</div>
+	);
+}
+
+export default Feature;
