81887b8314bb7648da7f5e457a2f3af7983792cb
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-11-26 14:16:45 +0700

    Imported required components in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,9 @@ import { NextPage } from "next";
 
 import Head from "next/head";
 
+import Headline from 'components/home_page/Headline';
+import Features from 'components/home_page/Features';
+import LessonMaterials from 'components/home_page/LessonMaterials';
 
 const Home: NextPage = () => {
 	return (
