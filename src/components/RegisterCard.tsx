8d07f4bd5ce99e54aeb1d918bc7d17daadc98c61
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-11-26 23:49:25 +0700

    Created register card component

---
+++ src/components/RegisterCard.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Link from 'next/link';
+
+const RegisterCard = () => {
+	return (
+		<div className="flex justify-center items-center gap-24 p-7 bg-cool-blue-800 rounded-lg">
+			<p className="text-white text-xl">Knowledge for free</p>
+
+			<Link className="button-primary" href="/">
+				Register {' '}
+				<span className="font-inter">&rarr;</span>
+			</Link>
+		</div>
+	);
+}
+
+export default RegisterCard;
