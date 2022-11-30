b7b8e882b1f1edcacf55b59cd1b57fba22c5ca3d
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-01 00:02:32 +0700

    Created headline for register page

---
+++ src/components/register_page/Headline.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Headline = () => {
+	return (
+		<header>
+			<div className="flex justify-center pt-44 pb-24">
+				<h1 className="text-3xl font-semibold text-center">Thank you for considering!</h1>
+			</div>
+		</header>
+	);
+}
+
+export default Headline;
