fc87832fb7c767e485ce2cbf1f6b4084d630f61e
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-01 19:54:19 +0700

    Created RadioQuestion component

---
+++ src/components/question/RadioQuestion.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import QuestionLabel from 'components/question/QuestionLabel';
+
+interface Option {
+	text: string;
+	value: string;
+}
+
+interface RadioQuestionProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
+	question: string;
+	options: Option[];
+	onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const RadioQuestion = ({ question, options, onChange, ...rest }: RadioQuestionProps) => {
+	return (
+		<QuestionLabel question={question}>
+			<div onChange={onChange} className="flex flex-col">
+				{options.map(option => {
+					return (
+						<label>
+							<input type="radio" value={option.value} {...rest} />
+							{' '}
+							<span>{option.text}</span>
+						</label>
+					);
+				})}
+			</div>
+		</QuestionLabel>
+	);
+}
+
+export default RadioQuestion;
