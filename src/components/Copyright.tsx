d2d4aee65523290d47394981c415eb7bb8837ee5
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-01 22:52:41 +0700

    Added br to Copyright component

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -1,7 +1,7 @@
 const Copyright = () => {
 	return (
 		<div>
-			<p className="text-gray-500">© 2022 Building Apps Program. All rights reserved.</p>
+			<p className="text-gray-500 text-center">© 2022 Building Apps Program. <br /> All rights reserved.</p>
 		</div>
 	);
 }
