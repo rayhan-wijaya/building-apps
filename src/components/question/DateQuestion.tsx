2aaee70c94cb4d38f945267cd8da0aba3185ac5c
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-01 19:54:33 +0700

    Created DateQuestion component

---
+++ src/components/question/DateQuestion.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import DatePicker, { ReactDatePickerProps } from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+import QuestionLabel from 'components/question/QuestionLabel';
+
+interface DateQuestionProps extends ReactDatePickerProps {
+	question: string;
+}
+
+const DateQuestion = ({ question, onChange, selected, ...rest }: DateQuestionProps) => {
+	return (
+		<QuestionLabel question={question}>
+			<DatePicker
+				dateFormat="dd/MM/yyyy"
+				onChange={onChange ?? (date => console.log(date))}
+				selected={selected}
+				adjustDateOnChange={true}
+				{...rest}
+			/>
+		</QuestionLabel>
+	);
+}
+
+export default DateQuestion;
