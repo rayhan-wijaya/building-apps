641250136b95773e67df270c442b166e41581b24
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-01 22:53:09 +0700

    Responsive register card

--- src/components/RegisterCard.tsx
+++ src/components/RegisterCard.tsx
@@ -4,10 +4,10 @@ import Link from 'next/link';
 
 const RegisterCard = () => {
 	return (
-		<div className="flex justify-center items-center gap-24 p-7 bg-cool-blue-800 rounded-lg">
+		<div className="flex flex-col sm:flex-row justify-center items-center gap-7 p-7 bg-cool-blue-800 rounded-lg">
 			<p className="text-white text-xl">Knowledge for free</p>
 
-			<Link className="button-primary" href="/register">
+			<Link className="button-primary sm:px-7 sm:py-3 px-12 py-3" href="/register">
 				Register {' '}
 				<span className="font-inter">&rarr;</span>
 			</Link>
