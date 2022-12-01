31bde9b001c89da8f9b9c30618cb1ce523163f16
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-01 22:15:58 +0700

    Home Page: Put link in LessonMaterial

--- src/components/home_page/LessonMaterial.tsx
+++ src/components/home_page/LessonMaterial.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface Props {
 	lessonMaterial: LessonMaterial;
@@ -9,12 +10,14 @@ interface Props {
 const LessonMaterial = ({ lessonMaterial }: Props) => {
 	return (
 		<div className="p-5 rounded-lg m-auto grayscale opacity-70 hover:filter-none hover:opacity-100 hover:shadow-lg transition-all">
-				<Image
-					src={`/lessonMaterials/${lessonMaterial.image}`}
-					alt={lessonMaterial.imageAlt}
-					height={200}
-					width={200}
-				/>
+				<Link href={lessonMaterial.href}>
+					<Image
+						src={`/lessonMaterials/${lessonMaterial.image}`}
+						alt={lessonMaterial.imageAlt}
+						height={200}
+						width={200}
+					/>
+				</Link>
 		</div>
 	);
 }
