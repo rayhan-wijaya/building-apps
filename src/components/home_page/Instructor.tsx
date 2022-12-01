e772176356ad4dc27b6d0f9f8bdec22b65959103
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-01 13:49:47 +0700

    Responsive Instructor component

--- src/components/home_page/Instructor.tsx
+++ src/components/home_page/Instructor.tsx
@@ -4,13 +4,13 @@ import Image from 'next/image';
 
 const Instructor = () => {
 	return (
-		<div className="flex item-center justify-center gap-14">
+		<div className="flex md:flex-row flex-col-reverse items-center justify-center gap-14">
 			<div className="flex flex-col justify-center">
 				<h2 className="text-ice-blue-900 font-semibold text-3xl mb-5">Hi! I'm Rayhan Wijaya</h2>
 				<p className="text-lg">I will be your instructor during <br /> this program.</p>
 			</div>
 
-			<div className="rounded-full overflow-hidden">
+			<div className="rounded-full overflow-hidden inline-block">
 				<Image
 					src="/instructor/rayhanwijaya.jpg"
 					alt="An image of Rayhan Wijaya."
