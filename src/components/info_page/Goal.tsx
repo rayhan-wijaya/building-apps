7cb53be3b5860e179b51e6768fba5d6a8ea79c42
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-04 00:03:06 +0700

    Info Page: Added Goal component

---
+++ src/components/info_page/Goal.tsx
@@ -0,0 +1,12 @@
+const Goal = () => {
+	return (
+		<div className="py-24 flex flex-col justify-center items-center">
+			<div className="text-lg text-center">
+				<p>This program aims to provide free education and training on building web and mobile apps.</p>
+				<p>Here are the chapters of this lesson:</p>
+			</div> 
+		</div>
+	);
+}
+
+export default Goal;
