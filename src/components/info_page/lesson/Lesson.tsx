b3955d08a5cf344f0c3f77a344eaf7f3d7ccf851
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-04 00:05:23 +0700

    Info Page: Created Lesson component

---
+++ src/components/info_page/lesson/Lesson.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface Props {
+	lesson: LessonMaterial;
+}
+
+const Lesson = ({ lesson }: Props) => {
+	return (
+		<Link href={lesson.href} legacyBehavior={true}>
+			<a className="grayscale opacity-70 hover:filter-none hover:opacity-100 hover:scale-105 hover:z-20 bg-white rounded-md hover:shadow-lg hover:shadow-gray-200 hover:border-gray-500 border-2 border-gray-200 transition-all p-7 flex flex-col justify-center gap-7 items-center">
+				<div className="h-44 w-full relative">
+					<Image
+						src={`/lessonMaterials/${lesson.image}`}
+						alt={`An icon of ${lesson.title}`}
+						fill={true}
+						objectFit="scale-down"
+					/>
+				</div>
+
+				<h3 className="text-2xl text-center">{lesson.title}</h3>
+			</a>
+		</Link>
+	);
+}
+
+export default Lesson;
