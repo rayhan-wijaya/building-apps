ee71e212157f9236912e8e05de2f2fd28330ae14
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-04 21:47:18 +0700

    Blog Page: BlogsHeadline component

---
+++ src/components/blog_page/BlogsHeadline.tsx
@@ -0,0 +1,14 @@
+const BlogsHeadline = () => {
+	return (
+		<header>
+			<div className="flex flex-col justify-center pt-56 pb-32 px-24 gap-3">
+				<h1 className="text-center font-semibold text-2xl">Blog</h1>
+				<p className="text-center text-lg">
+					The latest news about the Building Apps Program
+				</p>
+			</div>
+		</header>
+	);
+}
+
+export default BlogsHeadline;
