b17fcd05e594bf1f9443046867807f889d48a191
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-04 21:50:51 +0700

    Blog Page: Page for Listing Blog Posts

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,43 @@
+import { NextPage, GetStaticProps } from 'next';
+import Head from 'next/head';
+
+import getBlogPosts from 'utils/blogs/getBlogPosts';
+
+import BlogsHeadline from 'components/blog_page/BlogsHeadline';
+import Footer from 'components/Footer';
+
+import BlogPosts from 'components/blog_page/blogPost/BlogPosts';
+
+interface Props {
+	blogPosts: BlogPost[];
+}
+
+const Blogs: NextPage<Props> = ({ blogPosts }) => {
+	return (
+		<>
+			<Head>
+				<title>Blog • Building Apps</title>
+			</Head>
+
+			<main>
+				<BlogsHeadline />
+
+				<BlogPosts blogPosts={blogPosts} className="mb-24" />
+
+				<Footer />
+			</main>
+		</>
+	);
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+	const blogPosts = await getBlogPosts({ sortBy: 'date' });
+
+	return {
+		props: {
+			blogPosts,
+		}
+	};
+}
+
+export default Blogs;
