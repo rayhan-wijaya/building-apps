8b940ae265fb59c8dcf0102596dcacc01907ff14
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-04 21:47:44 +0700

    Blog Page: BlogPosts component

---
+++ src/components/blog_page/blogPost/BlogPosts.tsx
@@ -0,0 +1,20 @@
+import BlogPost from 'components/blog_page/blogPost/BlogPost';
+
+interface Props {
+	blogPosts: BlogPost[];
+	className?: string;
+}
+
+const BlogPosts = ({ blogPosts, className }: Props) => {
+	return (
+		<div className={`flex flex-col gap-4 justify-center items-center ${className}`}>
+			{blogPosts.map(blogPost => {
+				return (
+					<BlogPost blogPost={blogPost} />
+				);
+			})}
+		</div>
+	);
+}
+
+export default BlogPosts;
