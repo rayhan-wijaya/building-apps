3fd685bb7562a9f73bafbbe3fbf8f0194ba071f5
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-04 22:43:30 +0700

    Info Page: Added more goals

--- src/components/info_page/Goal.tsx
+++ src/components/info_page/Goal.tsx
@@ -3,6 +3,13 @@ const Goal = () => {
 		<div className="py-24 flex flex-col justify-center items-center">
 			<div className="text-lg text-center">
 				<p>This program aims to provide free education and training on building web and mobile apps.</p>
+				<p>
+					The goal is for the participants to be able to create websites, web
+					apps and mobile applications as tools for business
+					development, product marketing and selling, writing
+					articles, and increasing knowledge in the field of
+					technology, especially in the Information Technology field.
+				</p>
 				<p>Here are the chapters of this lesson:</p>
 			</div> 
 		</div>
