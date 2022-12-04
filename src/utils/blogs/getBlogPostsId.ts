5cf1ad94df138a1dfcb1c22d87c9e639d5652a2b
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-04 21:45:14 +0700

    Blog Page: getBlogPostsId util func

---
+++ src/utils/blogs/getBlogPostsId.ts
@@ -0,0 +1,25 @@
+import path from 'path';
+import fs from 'fs/promises';
+
+interface Params {
+	forGetStaticPaths?: boolean;
+}
+
+const getBlogPostsId = async ({ forGetStaticPaths }: Params) => {
+	const blogsDir = path.join(process.cwd(), 'data', 'blogs');
+	const postNames = await fs.readdir(blogsDir);
+
+	const postIds = postNames.map(postName => postName.replace('/\.md$/', ''));
+
+	if (forGetStaticPaths) {
+		return postIds.map(postId => ({
+			params: {
+				id: postId,
+			}
+		}));
+	}
+
+	return postIds;
+}
+
+export default getBlogPostsId;
