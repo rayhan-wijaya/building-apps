37e56be156658e67c9d264b2a01d96f1ac3cc301
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-06 15:47:43 +0700

    Info Page: Responsive LessonChapter

--- src/components/info_page/lesson/LessonChapter.tsx
+++ src/components/info_page/lesson/LessonChapter.tsx
@@ -9,7 +9,7 @@ const LessonChapter = ({ lessonChapter }: Props) => {
 		<div>
 			<h2 className="text-2xl font-semibold text-center mb-7">{lessonChapter.title}</h2>
 
-			<div className="grid grid-cols-3 gap-5">
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
 				{lessonChapter.lessons.map(lesson => {
 					return (
 						<Lesson lesson={lesson} />
