e9ac5583ad910bbc7b17e01abeec3422c6c9277e
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-06 15:46:36 +0700

    Info Page: 64px-width Lesson component

--- src/components/info_page/lesson/Lesson.tsx
+++ src/components/info_page/lesson/Lesson.tsx
@@ -8,7 +8,7 @@ interface Props {
 const Lesson = ({ lesson }: Props) => {
 	return (
 		<Link href={lesson.href} legacyBehavior={true}>
-			<a className="grayscale opacity-70 hover:filter-none hover:opacity-100 hover:scale-105 hover:z-20 bg-white rounded-md hover:shadow-lg hover:shadow-gray-200 hover:border-gray-500 border-2 border-gray-200 transition-all p-7 flex flex-col justify-center gap-7 items-center">
+			<a className="grayscale opacity-70 hover:filter-none hover:opacity-100 hover:scale-105 hover:z-20 bg-white rounded-md hover:shadow-lg hover:shadow-gray-200 hover:border-gray-500 border-2 border-gray-200 transition-all p-7 flex flex-col justify-center gap-7 items-center w-64">
 				<div className="h-44 w-full relative">
 					<Image
 						src={`/lessons/${lesson.image}`}
