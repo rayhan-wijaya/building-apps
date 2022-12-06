1c13f76f600b36ef7d7d6526c2681d9ae76168e5
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-07 00:22:43 +0700

    Included navbar at pages/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,16 @@ import { AppType } from "next/app";
 
 import { trpc } from "../utils/trpc";
 
+import Navbar from 'components/Navbar';
+
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-	return <Component {...pageProps} />;
+	return <>
+		<Navbar />
+
+		<Component {...pageProps} />
+	</>;
 };
 
 export default trpc.withTRPC(MyApp);
