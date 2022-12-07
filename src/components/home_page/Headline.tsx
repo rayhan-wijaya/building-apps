818f4ccfc3df1c4c90c8467d300d9ecf85f745bc
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-07 15:15:10 +0700

    Home Page: Responsive Headline

--- src/components/home_page/Headline.tsx
+++ src/components/home_page/Headline.tsx
@@ -8,36 +8,36 @@ import CodeWidget from 'components/home_page/CodeWidget';
 const Headline = () => {
 	return (
 		<header className="bg-cool-blue-900">
-			<div className="flex justify-center py-64 px-24 gap-16">
-				<div className="flex flex-col justify-center">
-					<h1 className="text-white text-3xl font-semibold mb-5">
+			 <div className="flex flex-col-reverse lg:flex-row justify-center py-64 px-8 lg:px-24 gap-32">
+				<div className="flex flex-col justify-center items-left xs:items-center lg:items-start">
+					<h1 className="text-white text-left xs:text-center lg:text-left text-3xl font-semibold mb-5">
 						Learn to Build Apps <br />
 						<span className="text-ice-blue-100 font-bold">For Free!</span>
 					</h1>
 
-					<p className="text-white mb-5 max-w-md">
+					<p className="text-white max-w-md mb-5 text-xl xs:text-center lg:text-left">
 						Teaching you mobile and web development.
 						At the end, you can build your own online shop!
 					</p>
 
-					<div className="flex gap-4">
-						<Link className="button-primary" href="/register">
+					<div className="flex flex-col lg:flex-row gap-3 justify-center items-start xs:items-center lg:items-start lg:justify-start text-lg w-full">
+						<Link className="button-primary lg:w-fit w-44 text-center text-xl py-2 whitespace-nowrap" href="/register">
 							Register {' '}
 							<span className="font-inter">&rarr;</span>
 						</Link>
 
-						<Link className="button-secondary" href="/info">More Info</Link>
+						<Link className="button-secondary lg:w-fit w-44 text-center text-xl py-2 whitespace-nowrap" href="/info">More Info</Link>
 					</div>
 				</div>
 
-				<div className="flex gap-5 items-center overflow-hidden relative">
-					<CodeWidget className="absolute w-full motion-safe:animate-codewidget" />
+				<div className="flex gap-5 justify-center items-center overflow-hidden relative">
+					<CodeWidget className="absolute w-full motion-safe:animate-codeWidget" />
 
 					<div className="opacity-0 motion-safe:animate-phone">
 						<Image src="/graphics/phone.svg" alt="An image of a product page on a phone." width={200} height={100} />
 					</div>
 					
-					<div className="opacity-0 motion-safe:animate-laptop">
+					<div className="opacity-0 motion-safe:animate-laptop hidden sm:block">
 						<Image src="/graphics/laptop.svg" alt="An image of a product page on a laptop." width={500} height={500} />
 					</div>
 				</div>
