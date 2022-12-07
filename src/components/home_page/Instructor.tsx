ff58fd0fd996cb358e2337ce94afdca5a541f963
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-07 15:45:32 +0700

    Home Page: Enlarged Instructor text

--- src/components/home_page/Instructor.tsx
+++ src/components/home_page/Instructor.tsx
@@ -7,7 +7,7 @@ const Instructor = () => {
 		<div className="flex md:flex-row flex-col-reverse items-center justify-center gap-14 px-8">
 			<div className="flex flex-col justify-center">
 				<h2 className="text-ice-blue-900 font-semibold text-3xl mb-5">Hi! I'm Rayhan Wijaya</h2>
-				<p className="text-lg">I will be your instructor during this program.</p>
+				<p className="text-xl">I will be your instructor during this program.</p>
 			</div>
 
 			<div className="rounded-full overflow-hidden inline-block">
