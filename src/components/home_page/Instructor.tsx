b8f1a4e490190e55ff335948467f0e5aca64d066
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-07 15:37:51 +0700

    Home Page: Added vertical padding to Instructor

--- src/components/home_page/Instructor.tsx
+++ src/components/home_page/Instructor.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 const Instructor = () => {
 	return (
-		<div className="flex md:flex-row flex-col-reverse items-center justify-center gap-14">
+		<div className="flex md:flex-row flex-col-reverse items-center justify-center gap-14 px-8">
 			<div className="flex flex-col justify-center">
 				<h2 className="text-ice-blue-900 font-semibold text-3xl mb-5">Hi! I'm Rayhan Wijaya</h2>
 				<p className="text-lg">I will be your instructor during <br /> this program.</p>
