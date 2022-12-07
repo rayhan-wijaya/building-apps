8ac8c52b9598100435ea9bbd2d2fa7ea5ac7c3d1
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-07 22:09:51 +0700

    DateQuestion: Type-imported ReactDatePickerProps

--- src/components/question/DateQuestion.tsx
+++ src/components/question/DateQuestion.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import DatePicker, { ReactDatePickerProps } from 'react-datepicker';
+import DatePicker from 'react-datepicker';
+import type { ReactDatePickerProps } from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 import QuestionLabel from 'components/question/QuestionLabel';
