1d9a1d41805c67d91affc1555e13393221fd480a
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-07 15:25:08 +0700

    Info Page: Enlarged goal paragraph

--- src/components/info_page/Goal.tsx
+++ src/components/info_page/Goal.tsx
@@ -1,7 +1,7 @@
 const Goal = () => {
 	return (
 		<div className="py-24 flex flex-col justify-center items-center">
-			<div className="text-lg max-w-3xl flex flex-col gap-5 px-14">
+			<div className="text-xl max-w-3xl flex flex-col gap-5 px-14">
 				<p>This program aims to provide free education and training on building web and mobile apps.</p>
 				<p>
 					The goal is for the participants to be able to create websites, web
