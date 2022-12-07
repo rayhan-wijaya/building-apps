7a7d42a959ff563dd5992f67836ba9f5b712dfd1
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-07 22:11:36 +0700

    Register Page: Escape apostrophe at Success

--- src/components/register_page/Success.tsx
+++ src/components/register_page/Success.tsx
@@ -13,7 +13,7 @@ const Success = () => {
 		<div className="flex flex-col items-center gap-12 pt-72 pb-64">
 			<div className="flex flex-col items-center gap-2">
 				<h1 className="font-semibold text-3xl text-center">Thank you so much for joining! &#10084;</h1>
-				<p className="text-lg text-center">Please join this program's official WhatsApp group.</p>
+				<p className="text-lg text-center">Please join this program&apos;s official WhatsApp group.</p>
 			</div>
 
 			<Link href={inviteLink} legacyBehavior={true}>
