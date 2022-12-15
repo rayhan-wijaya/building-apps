9220f7282ba945eba56fd26e851ae646d2549e83
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-15 22:42:47 +0700

    info page: Updated LessonChapters

--- src/components/info_page/lesson/LessonChapters.tsx
+++ src/components/info_page/lesson/LessonChapters.tsx
@@ -6,11 +6,12 @@ interface Props {
 
 const LessonChapters = ({ lessonChapters }: Props) => {
 	return (
-		<div className="flex flex-col gap-32 justify-center items-center p-12">
-			{lessonChapters.map(lessonChapter => {
+		<div className="flex flex-col gap-16 justify-center items-center p-12">
+			{lessonChapters.map((lessonChapter, lessonChapterIndex) => {
 				return (
 					<LessonChapter
 						lessonChapter={lessonChapter}
+						lessonChapterIndex={lessonChapterIndex + 1}
 						key={lessonChapter.title}
 					/>
 				);
