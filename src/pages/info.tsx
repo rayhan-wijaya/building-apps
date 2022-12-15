835edfd0c6f6ad75619f26b7709060e8f921c98b
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-15 22:45:01 +0700

    Info Page: Used LessonImages

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -2,6 +2,7 @@ import type { NextPage, GetStaticProps } from 'next';
 import Head from 'next/head';
 
 import getLessonChapters from 'utils/lessons/getLessonChapters';
+import getUniqueLessons from 'utils/lessons/getUniqueLessons';
 
 import Headline from 'components/info_page/Headline';
 import Footer from 'components/Footer';
@@ -9,12 +10,14 @@ import Footer from 'components/Footer';
 import Goal from 'components/info_page/Goal';
 
 import LessonChapters from 'components/info_page/lesson/LessonChapters';
+import LessonImages from 'components/info_page/lesson/LessonImages';
 
 interface Props {
 	lessonChapters: LessonChapter[];
+	uniqueLessons: Lesson[];
 }
 
-const MoreInfo: NextPage<Props> = ({ lessonChapters }) => {
+const MoreInfo: NextPage<Props> = ({ lessonChapters, uniqueLessons }) => {
 	return (
 		<>
 			<Head>
@@ -30,6 +33,8 @@ const MoreInfo: NextPage<Props> = ({ lessonChapters }) => {
 
 				<section>
 					<LessonChapters lessonChapters={lessonChapters} />
+
+					<LessonImages lessons={uniqueLessons} />
 				</section>
 
 				<Footer />
@@ -40,10 +45,12 @@ const MoreInfo: NextPage<Props> = ({ lessonChapters }) => {
 
 export const getStaticProps: GetStaticProps = async () => {
 	const lessonChapters = await getLessonChapters();
+	const uniqueLessons = await getUniqueLessons();
 
 	return {
 		props: {
 			lessonChapters,
+			uniqueLessons,
 		}
 	};
 }
