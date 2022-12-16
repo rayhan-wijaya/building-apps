39a3c671060f28e5e3119d52a82d627d3c2a1be3
rayhan-wijaya <rayhanwijaya210@gmail.com>
2022-12-16 17:28:05 +0700

    Used custom line numbers at CodeWidget

--- src/components/home_page/CodeWidget.tsx
+++ src/components/home_page/CodeWidget.tsx
@@ -7,7 +7,7 @@ interface ComponentProps {
 }
 
 const Component = ({ name, className, hasAnimation }: ComponentProps) => {
-	let formattedClassName = 'whitespace-no-wrap overflow-hidden text-white';
+	let formattedClassName = '';
 
 	if (hasAnimation) {
 		formattedClassName += ' w-0 animate-typing';
@@ -18,8 +18,10 @@ const Component = ({ name, className, hasAnimation }: ComponentProps) => {
 	}
 
 	return (
-		<pre className={formattedClassName}>
-			&lt;<span className="text-yellow-400">{name}</span> /&gt;
+		<pre className={`whitespace-nowrap overflow-hidden ${formattedClassName}`}>
+	    	<span className="text-white">&lt;</span>
+	    	<span className="text-yellow-400">{name}</span> {' '}
+	    	<span className="text-white">/&gt;</span>
 		</pre>
 	);
 }
@@ -29,19 +31,35 @@ interface Props {
 }
 
 const CodeWidget = ({ className }: Props) => {
+	const components = [
+		'ProductName',
+		'ProductImage',
+		' ',
+		'Reviews',
+		'Price',
+		'Buttons',
+	];
+
 	return (
-		<div className={`flex flex-col bg-slate-800 rounded-xl py-3 pl-14 pr-36 shadow-[inset_0px_1px_1px] shadow-slate-400 border-2 border-slate-900 ${className}`}>
-			<ol className="text-gray-500 font-mono list-decimal text-xl w-fit">
-				<li> <Component name="ProductName" hasAnimation={true} /> </li>
-				<li> <Component name="ProductImage" hasAnimation={true} /> </li>
+		<div className={`flex gap-3 bg-slate-800 rounded-xl py-5 pl-5 pr-36 shadow-[inset_0px_1px_1px] shadow-slate-400 border-2 border-slate-900 ${className}`}>
+			<div className="flex flex-col text-xl text-right text-gray-500 font-mono">
+				{components.map((_, i) => {
+					return (
+						<pre key={i}>{i + 1}</pre>
+					);
+				})}
+			</div>
 
-				<li>
-				</li>
+			<div className="text-gray-500 font-mono list-decimal text-xl w-fit">
+				<Component name="ProductName" hasAnimation={true} />
+				<Component name="ProductImage" hasAnimation={true} />
 
-				<li> <Component name="Reviews" hasAnimation={true} /> </li>
-				<li> <Component name="Price" hasAnimation={true} /> </li>
-				<li> <Component name="Buttons" hasAnimation={true} /> </li>
-			</ol>
+				<pre>{' '}</pre>
+
+				<Component name="Reviews" hasAnimation={true} />
+				<Component name="Price" hasAnimation={true} />
+				<Component name="Buttons" hasAnimation={true} />
+			</div>
 		</div>
 	);
 }
