6a2aea6f01e05f23d276823fda5862b684d18ae7
Rayhan Wijaya <118240016+rayhan-wijaya@users.noreply.github.com>
2022-12-19 16:14:04 +0700

    Register Page: Loading component

---
+++ src/components/register_page/Loading.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+
+interface LoadingComponents {
+    CheckingResponses: React.FC;
+}
+
+const Loading: LoadingComponents = {
+    CheckingResponses: () => {
+        useEffect(() => {
+            window.scrollTo(0, 0);
+        }, []);
+
+        return (
+            <div className="flex flex-col items-center gap-12 pt-72 pb-64">
+		    	<div className="flex flex-col items-center gap-5">
+		    		<h1 className="font-semibold text-3xl text-center">Checking Responses..</h1>
+
+		    		<p className="text-lg text-center">Please wait while we check the registration responses</p>
+		    	</div>
+            </div>
+        );
+    }
+}
