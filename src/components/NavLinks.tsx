4a6cf91ed25d6564a9913bbfbf715b6e8740ab54
Rayhan Wijaya <118240016+rayhan-wijaya@users.noreply.github.com>
2022-12-19 16:21:24 +0700

    NavLinks: Displaying home link only when the screen is small

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const NavLinks = () => {
 	return (
 		<>
-		 	<Link href="/" className="text-lg">Home</Link>
+		 	<Link href="/" className="text-lg block sm:hidden">Home</Link>
 			<Link href="/info" className="text-lg">More Info</Link>
 			<Link href="/blog" className="text-lg">Blog</Link>
 		</>
